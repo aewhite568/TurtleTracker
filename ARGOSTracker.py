a989bed632877cc306270472e35e35c2020e5386
aewhite568 <aw337@duke.edu>
2019-09-30 12:11:01 -0400

    Adds front matter

---
+++ ARGOSTracker.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Audrey White (audrey.white@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------

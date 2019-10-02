e0bef92bd25e7dbafbfe7d9352740dccfbf7a551
aewhite568 <aw337@duke.edu>
2019-10-02 12:43:52 -0400

    Adds message if no date found

--- ARGOSTracker.py
+++ ARGOSTracker.py
@@ -46,7 +46,6 @@ for lineString in lineStrings:
         dateDict[recordID] = obsDate
         locationDict[recordID] = (obsLat, obsLon)
 
-
 # Ask user for date
 userDate = input("Enter a date (M/D/YYYY):")
 
@@ -56,10 +55,15 @@ for k,v in dateDict.items():
     if v == userDate:
         keyList.append(k)
 
-# Loop through each key and report the associated date location
-for k in keyList:
-    theDate = dateDict[k]
-    theLocation = locationDict[k]
-    theLat = theLocation[0]
-    theLon = theLocation[1]
-    print("Record {0}: Sara was see at {1}N-{2}W, on {3}".format(k,theLat,theLon,theDate))
+# Check that at least one key was returned; tell the user if not.
+if len(keyList) == 0:
+    print ("No observations recorded for {}".format(userDate))
+else:
+    # Loop through each key and report the associated date location
+    for k in keyList:
+        theDate = dateDict[k]
+        theLocation = locationDict[k]
+        theLat = theLocation[0]
+        theLon = theLocation[1]
+        print("Record {0}: Sara was see at {1}N-{2}W, on {3}".format(k,theLat,theLon,theDate))
+    
